47cb155791587caf0d8aef80b0971610d7b93a32
nicklasjepsen <nicklas.m.jepsen@gmail.com>
2017-03-31 23:56:39 +0200

    Added unit test

---
+++ ProfileApi.Tests/PeopleControllerUnitTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ProfileApi.WebApi.Controllers;
+using ProfileApi.WebApi.Data;
+using ProfileApi.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProfileApi.Tests
+{
+    [TestClass]
+    public class PeopleControllerUnitTests
+    {
+        private Mock<IPersonRepository> repositoryContext;
+        private PeopleController subject;
+
+        [TestMethod]
+        public async Task TestGet()
+        {
+            var id = 1;
+            repositoryContext = new Mock<IPersonRepository>();
+            repositoryContext.Setup(mock => mock.FindAsync(It.IsAny<int>())).Returns(Task.Run(() => new Person
+            {
+                Id = id
+            }));
+            subject = new PeopleController(repositoryContext.Object);
+
+            var result = await subject.Get(1) as OkObjectResult;
+            
+            Assert.IsNotNull(result?.Value);
+            var person = result?.Value as Person;
+            Assert.IsNotNull(person);
+            Assert.AreEqual(id, person?.Id);
+        }
+    }
+}
