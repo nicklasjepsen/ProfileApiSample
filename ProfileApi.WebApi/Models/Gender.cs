043bd44a7d116e2d02a3967da2f0307fc1a99363
nicklasjepsen <nicklas.m.jepsen@gmail.com>
2017-03-31 22:39:57 +0200

    Removed persons list from model

--- ProfileApi.WebApi/Models/Gender.cs
+++ ProfileApi.WebApi/Models/Gender.cs
@@ -9,6 +9,5 @@ namespace ProfileApi.WebApi.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public ICollection<Person> People { get; set; }
     }
 }
