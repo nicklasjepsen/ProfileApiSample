efa9ab9bd2d5cf553b89bd326c519e38b49ae00d
nicklasjepsen <nicklas.m.jepsen@gmail.com>
2017-03-31 21:46:33 +0200

    Base EF model and context

---
+++ ProfileApi.WebApi/Data/PersonContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProfileApi.WebApi.Models;
+
+namespace ProfileApi.WebApi.Data
+{
+    public class PersonContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+        public DbSet<Gender> Genders { get; set; }
+
+        public PersonContext(DbContextOptions<PersonContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // EF will use names of the DbSets to determine the table name.
+            // Here we use the singular form of the object in stead
+            modelBuilder.Entity<Person>().ToTable(nameof(Person));
+            modelBuilder.Entity<Gender>().ToTable(nameof(Gender));
+        }
+    }
+}
